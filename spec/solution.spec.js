b7440c75ffa91ab0eb97d2feefa2594007cac1de
joannacollier <collj035@gmail.com>
2020-10-21 10:21:40 +0100

    Update spec, template complete.

--- spec/solution.spec.js
+++ spec/solution.spec.js
@@ -1,8 +1,12 @@
-const sum = require('../src/solution.js');
+const solution = require('../src/solution.js');
 
 describe("", () =>{
 
+  // beforeEach(function() {
+
+  // });
+
   test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(solution(1, 2)).toBe(3);
   });
 });
